3edaa44df13bac4709e1dccb6f2705f782266b0b
aitor santaeugenia mari <asantaeugenia@uoc.edu>
2023-10-19 02:30:12 +0200

    fix: fixed ex3 entryCalculator and schedule, now getting data from data.js, before It wasn't

--- PEC2_Ej3/f_zoo/core.js
+++ PEC2_Ej3/f_zoo/core.js
@@ -1,10 +1,8 @@
+const data = require('./data');
+
 function entryCalculator(entrants) {
   // your code here
-  const prices = {
-    Adult: 49.99,
-    Child: 20.99,
-    Senior: 24.99
-  };
+  const prices = data.prices;;
     
   if(!entrants || Object.keys(entrants).length === 0){
     return 0;
@@ -23,31 +21,43 @@ function entryCalculator(entrants) {
 
 function schedule(dayName) {
   // your code here
-  const schedule = {Tuesday: 'Open from 8am until 6pm',
-                    Wednesday: 'Open from 8am until 6pm',
-                    Thursday: 'Open from 10am until 8pm',
-                    Friday: 'Open from 10am until 8pm',
-                    Saturday: 'Open from 8am until 10pm',
-                    Sunday: 'Open from 8am until 8pm',
-                    Monday: 'CLOSED'}
+  const schedule = data.hours;
 
-  if(!dayName ||dayName.length === 0){
-    return {Tuesday: 'Open from 8am until 6pm',
-    Wednesday: 'Open from 8am until 6pm',
-    Thursday: 'Open from 10am until 8pm',
-    Friday: 'Open from 10am until 8pm',
-    Saturday: 'Open from 8am until 10pm',
-    Sunday: 'Open from 8am until 8pm',
-    Monday: 'CLOSED'};
-
-  }else{
-    const horario = {[dayName] : schedule[dayName]};
-    return horario; 
+   if (schedule.hasOwnProperty(dayName)) {
+    const horario = schedule[dayName];
+     
+    if (horario.open === 0 && horario.close === 0) {
+      const closedDay = {};
+      closedDay[dayName] = 'CLOSED';
+      
+      return closedDay;
+    } else {
+      const openHours = horario.close <= 12 ? horario.close + 'am' : (horario.close - 12) + 'pm';
+      const openDay = {};
+      openDay[dayName] = `Open from ${horario.open}am until ${openHours}`;
+      
+      return openDay;
+    }
+  } else {
+    const readableSchedule = {};
+    
+    for (const dia in schedule) {
+      const horario = schedule[dia];
+      
+      if (horario.open === 0 && horario.close === 0) {
+        readableSchedule[dia] = 'CLOSED';
+      } else {
+        const openHours = horario.close <= 12 ? horario.close + 'am' : (horario.close - 12) + 'pm';
+        readableSchedule[dia] = `Open from ${horario.open}am until ${openHours}`;
+      }
+    }
+    return readableSchedule;
   }
 }
 
 function animalCount(species) {
   // your code here
+  console.log(species)
 }
 
 function animalMap(options) {
