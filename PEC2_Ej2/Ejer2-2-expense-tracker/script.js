d4edd4cf4aee157bab5caad0dfb75fea85ddf258
aitor santaeugenia mari <asantaeugenia@uoc.edu>
2023-10-22 11:56:18 +0200

    fix: modificació de script, tot i a que no ho utilitzem

--- PEC2_Ej2/Ejer2-2-expense-tracker/script.js
+++ PEC2_Ej2/Ejer2-2-expense-tracker/script.js
@@ -47,9 +47,11 @@ function addTransaction(e) {
 }
 
 // Generate random ID
+/*
 function generateID() {
   return Math.floor(Math.random() * 100000000);
 }
+*/
 
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
